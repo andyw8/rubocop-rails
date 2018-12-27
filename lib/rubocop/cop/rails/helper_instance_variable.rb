ba9bfeec92b629b2ebfd542065c2b2555e60d2ef
Andy Waite <andyw8@users.noreply.github.com>
2018-12-26 20:35:58 -0500

    wip

--- lib/rubocop/cop/rails/helper_instance_variable.rb
+++ lib/rubocop/cop/rails/helper_instance_variable.rb
@@ -3,87 +3,24 @@
 module RuboCop
   module Cop
     module Rails
-      # This cop checks dynamic `find_by_*` methods.
-      # Use `find_by` instead of dynamic method.
-      # See. https://github.com/rubocop-hq/rails-style-guide#find_by
+      # This cop checks for use of the Timecop gem.
+      # Rails 4.1 added TimeHelpers to ActiveSupport. This can
+      # replace most uses of the Timecop gem, meaning one less
+      # external dependency for your project.
       #
       # @example
       #   # bad
-      #   User.find_by_name(name)
-      #
-      #   # bad
-      #   User.find_by_name_and_email(name)
-      #
-      #   # bad
-      #   User.find_by_email!(name)
+      #   Timecop.freeze Time.now.utc
       #
       #   # good
-      #   User.find_by(name: name)
-      #
-      #   # good
-      #   User.find_by(name: name, email: email)
-      #
-      #   # good
-      #   User.find_by!(email: email)
+      #   travel_to Time.now.utc
       class HelperInstanceVariable < Cop
-        MSG = 'Use `%<static_name>s` instead of dynamic `%<method>s`.'.freeze
-        METHOD_PATTERN = /^find_by_(.+?)(!)?$/.freeze
-
+        MSG = 'Use the Rails built-in time helpers instead of Timecop.'.freeze
         def on_send(node)
-          require 'pry'; binding.pry
-          method_name = node.method_name.to_s
+          receiver, _method, _args = *node
+          return unless receiver && receiver.source == 'Timecop'
 
-          return if whitelist.include?(method_name)
-
-          static_name = static_method_name(method_name)
-
-          return unless static_name
-
-          add_offense(node,
-                      message: format(MSG, static_name: static_name,
-                                           method: node.method_name))
-        end
-
-        def autocorrect(node)
-          keywords = column_keywords(node.method_name)
-
-          return if keywords.size != node.arguments.size
-
-          lambda do |corrector|
-            autocorrect_method_name(corrector, node)
-            autocorrect_argument_keywords(corrector, node, keywords)
-          end
-        end
-
-        private
-
-        def autocorrect_method_name(corrector, node)
-          corrector.replace(node.loc.selector,
-                            static_method_name(node.method_name.to_s))
-        end
-
-        def autocorrect_argument_keywords(corrector, node, keywords)
-          keywords.each.with_index do |keyword, idx|
-            corrector.insert_before(node.arguments[idx].loc.expression, keyword)
-          end
-        end
-
-        def whitelist
-          cop_config['Whitelist']
-        end
-
-        def column_keywords(method)
-          keyword_string = method.to_s[METHOD_PATTERN, 1]
-          keyword_string.split('_and_').map { |keyword| "#{keyword}: " }
-        end
-
-        # Returns static method name.
-        # If code isn't wrong, returns nil
-        def static_method_name(method_name)
-          match = METHOD_PATTERN.match(method_name)
-          return nil unless match
-
-          match[2] ? 'find_by!' : 'find_by'
+          add_offense(node)
         end
       end
     end
--- spec/rubocop/cop/rails/helper_instance_variable_spec.rb
+++ spec/rubocop/cop/rails/helper_instance_variable_spec.rb
@@ -3,33 +3,16 @@
 RSpec.describe RuboCop::Cop::Rails::HelperInstanceVariable do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense when using `Timecop`' do
+  it 'registers an offense when using Timecop' do
     expect_offense(<<-RUBY.strip_indent)
-      Timecop
-      ^^^^^^^ Use Rails built-in time travel helpers instead of Timecop.
+      Timecop.foo
+      ^^^^^^^^^^^ Use the Rails built-in time helpers instead of Timecop.
     RUBY
   end
 
-  it 'registers an offense when using `#take`' do
-    expect_offense(<<-RUBY.strip_indent)
-      User.where(id: x).take
-           ^^^^^^^^^^^^^^^^^ Use `find_by` instead of `where.take`.
-    RUBY
-  end
+  Time.now
 
-  it 'does not register an offense when using find_by' do
-    expect_no_offenses('User.find_by(id: x)')
-  end
-
-  it 'autocorrects where.take to find_by' do
-    new_source = autocorrect_source('User.where(id: x).take')
-
-    expect(new_source).to eq('User.find_by(id: x)')
-  end
-
-  it 'does not autocorrect where.first' do
-    new_source = autocorrect_source('User.where(id: x).first')
-
-    expect(new_source).to eq('User.where(id: x).first')
+  it 'does not register an offense when using the built in time helpers' do
+    expect_no_offenses('travel_to(Date.tomorrow)')
   end
 end
